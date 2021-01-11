0b8be93e2e6bd195a56939686c3b995635cd2c00
Cameron Tanjoco <ctanjoco@gmail.com>
2021-01-11 11:10:33 -0800

    adding css rules to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,11 @@ module.exports = {
         exclude: /node_modules/,
         use: ["babel-loader"],
       },
+      {
+        test: /\.css$/,
+
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
     ],
   },
   resolve: {
