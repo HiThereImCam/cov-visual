76abbb65e7797cdcd82a281ece6c2cb11f8b9df1
Cameron Tanjoco <ctanjoco@gmail.com>
2021-01-11 08:18:41 -0800

    Added map and covid data

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
+import statesAbbr from "../config/states";
+
 let svg = d3.select("#svg");
 
 let states;
-
-let covidUsData;
 let covidStateData;
 
 axios.get("https://api.covidtracking.com/v1/us/current.json").then((data) => {
   covidUsData = data;
 });
 
+
 axios
   .get("https://api.covidtracking.com/v1/states/current.json")
   .then((data) => {
     covidStateData = data;
-    
+    drawMap();
   });
 
 let drawMap = () => {
@@ -23,15 +24,23 @@ let drawMap = () => {
     .enter()
     .append("path")
     .attr("d", d3.geoPath())
-    .attr("fill", ( data ) => {
-       let stateAbbr = 
+    .attr("fill", (statesItem) => {
+      let stateName = statesItem.properties.name;
+
+      let infected = covidStateData.data.find((covidItem) => {
+        return covidItem.state === statesAbbr[stateName];
+      });
+
+      if (infected != undefined && infected.positive > 1) {
+        return "red";
+      } else {
+        null;
+      }
     });
 };
 
 d3.json("https://cdn.jsdelivr.net/npm/us-atlas@3/states-albers-10m.json").then(
   (us) => {
     states = topojson.feature(us, us.objects.states).features;
-    console.log("states: ", states);
-    drawMap();
   }
 );
