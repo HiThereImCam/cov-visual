da11b9fbd252e2447d8d9bff32b98b2364de0025
Cameron Tanjoco <ctanjoco@gmail.com>
2021-04-28 22:59:54 -0700

    added gradient and added updtes to vax records obj

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import statePopulation from "../config/population";
 import legend from "../config/legend";
+import { html } from "htl";
 
-const height = 610;
-const width = 975;
+let height = 610;
+let width = 975;
+//let padding = 20;
 
 // map takes in an object and callback
 
@@ -12,9 +14,7 @@ let svg = d3
   .attr("width", width)
   .attr("height", height);
 
-let stateVaxRecords = {};
-
-window.stateRecords = stateVaxRecords;
+let active = d3.select(null);
 
 var path = d3.geoPath();
 
@@ -22,15 +22,61 @@ let usAlbersJson = d3.json(
   "https://cdn.jsdelivr.net/npm/us-atlas@3/states-albers-10m.json"
 );
 
+let stateVaxRecords = {};
+let previousDayStateTotalVax = {};
+let twoDaysAgoStateTotalVax = {};
+
+window.stateRecords = stateVaxRecords;
+window.previousDay = previousDayStateTotalVax;
+window.twoDaysAgo = twoDaysAgoStateTotalVax;
+// clear st
+
+let date;
+
 let stateVaxCSV = d3.csv(
   "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/us_state_vaccinations.csv",
   (stateVaxObj) => {
-    // console.log("stateVax: ", stateVaxObj);
+    // our world in data updates vaccine data at 12 PST everyday
+
     let formatTime = d3.timeFormat("%Y-%m-%d");
-    let currentDay = formatTime(new Date());
-    if (stateVaxObj.date == currentDay) {
-      // stateVaxRecords.set(stateVaxObj.location, stateVaxObj);
-      stateVaxRecords[stateVaxObj.location] = stateVaxObj;
+    let today = new Date();
+    let yesterday = new Date(today);
+
+    yesterday.setDate(yesterday.getDate() - 1);
+
+    let todayMinusTwo = new Date(today);
+    todayMinusTwo.setDate(todayMinusTwo.getDate() - 2);
+
+    let currentDay = formatTime(today);
+    let previousDay = formatTime(yesterday);
+    let twoDaysAgo = formatTime(todayMinusTwo);
+
+    // we don't know when the API updates. We just know it happens sometime between 12 and 1pm so
+    // I'm taking the max of the two to be safe.
+
+    if (today.getHours() < 13) {
+      if (stateVaxObj.date == twoDaysAgo) {
+        twoDaysAgoStateTotalVax[stateVaxObj.location] =
+          stateVaxObj.people_fully_vaccinated;
+      }
+
+      if (stateVaxObj.date == previousDay) {
+        previousDayStateTotalVax[stateVaxObj.location] =
+          stateVaxObj.people_fully_vaccinated;
+
+        stateVaxRecords[stateVaxObj.location] = stateVaxObj;
+      }
+    } else {
+      if (stateVaxObj.date == previousDay) {
+        console.log(stateVaxObj);
+
+        previousDayStateTotalVax[stateVaxObj.location] =
+          stateVaxObj.people_fully_vaccinated;
+      }
+
+      if (stateVaxObj.date == currentDay) {
+        stateVaxRecords[stateVaxObj.location] = stateVaxObj;
+      }
     }
   }
 );
@@ -47,97 +93,33 @@ svg
     legend({ color, title: "Vaccinated Population (%)", width: 260 })
   );
 
-// let x = d3.scaleLinear().domain([0, 10]);
-// .rangeRound([10, 20, 30, 40, 50, 60, 70, 80, 90]);
-
-// rangeRound([])
-// .rangeRound([600, 860]);
-
-//this deals with the width of the scale
-// var x = d3.scaleLinear().domain([0, 100]).range([300, 600]);
-
-// var x = d3.scaleLinear().domain([10, 100]).rangeRound([600, 860]);
-
-// let color = d3
-//   .scaleThreshold()
-//   .domain(d3.range(10, 100))
-//   .range(d3.schemePaired[10]);
-
-// let color = d3.scaleThreshold().domain([0, 100]).
-
-// let color = d3.scaleThreshold().domain([0, 10]).range(d3.schemeGreens[10]);
-
-// var g = svg
-//   .append("g")
-//   .attr("class", "key")
-//   .attr("transform", "translate(0,40)");
-
-// console.log("this is g: ", g);
-
-// g.selectAll("rect")
-//   .data(
-//     color.range().map(function (d) {
-//       console.log("THIS IS D: ", d);
-//       d = color.invertExtent(d);
-//       if (d[0] == null) d[0] = x.domain()[0];
-//       if (d[1] == null) d[1] = x.domain()[1];
-//       return d;
-//     })
-//   )
-//   .enter()
-//   .append("rect")
-//   .attr("height", 8)
-//   .attr("x", function (d) {
-//     console.log("What is d here: ", d);
-//     return x(d[0]);
-//   })
-//   .attr("width", function (d) {
-//     console.log("Is this a different D(?): ", d);
-//     return x(d[1]) - x(d[0]);
-//   })
-//   .attr("fill", function (d) {
-//     return color(d[0]);
-//   });
-
-// g.append("text")
-//   .attr("class", "caption")
-//   .attr("x", x.range()[0])
-//   .attr("y", -6)
-//   .attr("fill", "#000")
-//   .attr("text-anchor", "start")
-//   .attr("font-weight", "bold")
-//   .text("Vaccination rate");
-
-// g.call(
-//   d3
-//     .axisBottom(x)
-//     .tickSize(13)
-//     .tickFormat(function (x, i) {
-//       return i ? x : x + "%";
-//     })
-//     .tickValues(color.domain())
-//     .ticks(10)
-// )
-//   .select(".domain")
-//   .remove();
-
-// g in this case is the legend
+// create tooltip
+let tooltip = d3
+  .select("body")
+  .append("div")
+  .attr("class", "tooltip")
+  .style("position", "absolute")
+  .style("display", "flex")
+  .style("flex-direction", "column")
+  .style("align-items", "center")
+  .style("background-color", "white")
+  .style("border", "1px solid")
+  .style("border-radius", "5px")
+  .style("width", "300px")
+  .style("height", "82px")
+  .style("padding", "10px")
+  .style("visibility", "hidden");
 
 Promise.all([usAlbersJson, stateVaxCSV, statePopulationObj]).then((values) => {
   drawMap(values[0], stateVaxRecords, values[2]);
 });
 
-// Promise.all(apiUrls).then((urls) => console.log("this is urls: ", urls));
-
-let drawMap = (usTopoData, stateVaxObj, statePopulation) => {
-  console.log("this is usTopoData", usTopoData);
-  console.log("this is stateVaxObj", stateVaxObj);
-  console.log("this is state population: ", statePopulation);
-  console.log(
-    "topojson: ",
-    topojson.feature(usTopoData, usTopoData.objects.states).features
-  );
+let drawMap = (usTopoData, stateVax, statePopulation) => {
   try {
+    // new york === new york state in stateVaxObx
+
+    let statePercentage;
+    let stateTotalVaccinated;
     svg
       .append("g")
       .attr("class", "states")
@@ -148,25 +130,74 @@ let drawMap = (usTopoData, stateVaxObj, statePopulation) => {
       .attr("fill", (d) => {
         let d_State = d.properties.name;
 
-        // new york === new york state in stateVaxObx
         let percentage = statePopulation[d_State]
           ? d_State === "New York"
-            ? (stateVaxObj["New York State"].people_fully_vaccinated /
+            ? (stateVax["New York State"].people_fully_vaccinated /
                 statePopulation[d_State]) *
               100
-            : (stateVaxObj[d_State].people_fully_vaccinated /
+            : (stateVax[d_State].people_fully_vaccinated /
                 statePopulation[d_State]) *
               100
           : 0;
 
+        // let percentageIncrease = statePopulation[d_State] ? d_State === "New York"
+
         // turning percentage into a float with 2 decimal points
         percentage = Number.parseFloat(percentage).toPrecision(4);
-        console.log("this is percentage: ", percentage);
+
+        d.properties.name = d_State;
+        d.properties.percentage = percentage;
+        d.properties.statePopulation = statePopulation[d_State];
+        d.properties.date = date;
+        // d.properties.previousDay = priorDay;
+
+        d_State === "New York"
+          ? (d.properties.fully_vaccinated =
+              stateVax["New York State"].people_fully_vaccinated)
+          : (d.properties.fully_vaccinated =
+              stateVax[d_State].people_fully_vaccinated);
+
         let col = color(percentage);
-        console.log("This is col: ", col);
+
         return col;
       })
-      .attr("d", path);
+      .attr("d", path)
+      .on("mouseover", (d) => {
+        let {
+          name,
+          percentage,
+          statePopulation,
+          fully_vaccinated,
+          date,
+        } = d.properties;
+
+        tooltip.transition().duration(200).style("opacity", 0.9);
+        tooltip.html(`
+         <div
+         style="position: absolute;  display: flex; justify-content: center; align-items: center;
+          flex-direction: column;"
+          >
+            <span>${name} (as of ${date})</span>
+            <span>Population (as of 2019): ${statePopulation}</span>
+            <span>Number of people vaccinated: ${fully_vaccinated}</span>
+            <span>Percentage of people vaccinated: ${percentage} %</span>
+            <span>Percentage increase from previous day ()
+          </div>
+          `);
+      })
+      .on("mouseout", (d) => {
+        d3.select(".tooltip")
+          .transition()
+          .duration(200)
+          .style("visibility", "visible")
+          .style("opacity", 0);
+      })
+      .on("mousemove", () => {
+        d3.select(".tooltip")
+          .style("visibility", "visible")
+          .style("left", d3.event.pageX + "px")
+          .style("top", d3.event.pageY + "px");
+      });
 
     svg
       .append("path")
@@ -174,9 +205,12 @@ let drawMap = (usTopoData, stateVaxObj, statePopulation) => {
         topojson.mesh(usTopoData, usTopoData.objects.states, (a, b) => a !== b)
       )
       .attr("fill", "none")
-      .attr("stroke", "navy")
+      .attr("stroke", "#485063")
+      // .attr("stroke", "white")
       .attr("stroke-linejoin", "round")
       .attr("d", path);
+
+    d3.select("svg").selectAll(".states");
   } catch (e) {
     console.log("error: ", e);
   }
